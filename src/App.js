2b0a6521ccc310610fff71e61191a1603d0b8b62
Alperen Özkan <alperenweb3@gmail.com>
2023-03-02 17:00:15 +0100

    data.js added to the project, card component is updated via props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import "./App.css";
+import data from "./data";
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Headline from "./components/Headline";
 import Card from "./components/Card";
 
 function App() {
+  const Cards = data.map((CardData) => {
+    return <Card data={CardData} />;
+  });
   return (
     <div className="App">
       <Header />
       <Hero />
       <Headline />
-      <Card />
+      <div className="container">
+        <div className="card__wrapper">{Cards}</div>
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,37 +1,42 @@
 import React from "react";
 import "./styles/Card.css";
-import swimmer from "./images/swimmer.png";
 import star from "./images/star.svg";
 
-function Card() {
+function Card(props) {
+  let cardStatus;
+  if(props.data.openSpots === 0){
+    cardStatus = "Sold Out";
+  }
+  else{
+    cardStatus = "Online";
+  }
+
   return (
-    <div className="container">
-      <div className="card__wrapper">
+
         <div className="card">
 
           <div className="card__img-container">
-            <img src={swimmer} alt="Swimmer" className="card__img" />
-            <div className="card__status">sold out</div>
+            <img src={process.env.PUBLIC_URL + "/images/" + props.data.coverImg} alt="Swimmer" className="card__img" />
+            <div className="card__status">{cardStatus}</div>
           </div>
 
           <div className="card__body">
 
             <div className="card__rating">
               <img src={star} alt="" className="card__star" />
-              <span className="card__rating-point">5.0</span>
-              <span className="card__rating-comment-count">(6)</span>
-              <span className="card__location">USA</span>
+              <span className="card__rating-point">{props.data.stats.rating}</span>
+              <span className="card__rating-comment-count">({props.data.stats.reviewCount})</span>
+              <span className="card__location">{props.data.location}</span>
             </div>
 
-            <p className="card__title">Life lessons with Katie Zaferes</p>
+            <p className="card__title">{props.data.title}</p>
             <p className="card__price">
-              From $136<span className="card__price-for"> / person</span>
+              From ${props.data.price}<span className="card__price-for"> / person</span>
             </p>
           </div>
 
         </div>
-      </div>
-    </div>
+
   );
 }
 
