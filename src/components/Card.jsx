15cb52c0437c4a2680e1e0a60d840e8d073afd2b
Alperen Özkan <alperenweb3@gmail.com>
2023-03-02 14:14:14 +0100

    card component created with styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import "./styles/Card.css";
+import swimmer from "./images/swimmer.png";
+import star from "./images/star.svg";
 
 function Card() {
   return (
-    <div className="container card">
+    <div className="container">
+      <div className="card__wrapper">
+        <div className="card">
+
+          <div className="card__img-container">
+            <img src={swimmer} alt="Swimmer" className="card__img" />
+            <div className="card__status">sold out</div>
+          </div>
+
+          <div className="card__body">
+
+            <div className="card__rating">
+              <img src={star} alt="" className="card__star" />
+              <span className="card__rating-point">5.0</span>
+              <span className="card__rating-comment-count">(6)</span>
+              <span className="card__location">USA</span>
+            </div>
+
+            <p className="card__title">Life lessons with Katie Zaferes</p>
+            <p className="card__price">
+              From $136<span className="card__price-for"> / person</span>
+            </p>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
